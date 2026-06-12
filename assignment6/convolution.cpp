26979062c5458d653109e3ef73f5ecb429668f99
taiseiyasuno <taisei.yas@gmail.com>
2026-06-12 14:29:52 +0100

    assignment6

---
+++ assignment6/convolution.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <vector>
+
+// let's do a 1D implementation first
+
+
+class Matrix {
+    std::vector<std::vector<int>> mat; 
+    
+}
+
+
+using Matrix = std::vector<std::vector<float>>;
+
+// input size (H x W x D, e.g. 28 x 28 x 1 input for 1 channel) 
+// kernel size (). A convolutional filter will shrink the original dimensions: (H - ())
+
+int main(){
+    srand(0);
+    
+
+}
---
+++ assignment6/test.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+a = np.random.rand(3,1)
+print(np.shape(a)[1])
